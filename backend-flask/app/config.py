241f37c42ff7654d8fc5b5a9556095520e511864
Omar Reyes L. <relo2210@gmail.com>
2026-02-07 17:23:42 -0600

    feat: Enhance configuration to load environment variables and set database URI

--- backend-flask/app/config.py
+++ backend-flask/app/config.py
@@ -1,5 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    secret_key = os.getenv('SECRET_KEY') or 'Hackathon2026'
-    
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
