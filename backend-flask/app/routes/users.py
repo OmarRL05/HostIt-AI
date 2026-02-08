ffe7befa6aee259db4f481c238ed66f91561e587
Omar Reyes L. <relo2210@gmail.com>
2026-02-08 02:58:16 -0600

    feat: Add user profile management endpoints
    - Implement GET endpoint to retrieve user profile details
    - Implement PUT endpoint to update user profile information
    - Enhance database interactions for user data management

---
+++ backend-flask/app/routes/users.py
@@ -0,0 +1,40 @@
+from collections import UserDict
+from turtle import st
+from flask import Blueprint, request, jsonify
+from ..extensions import db
+from ..models import User
+
+user_bp = Blueprint('user', __name__)
+
+# to get profile details
+@user_bp.route('/api/users/<int:user_id>', methods=['GET'])
+def get_user_profile(user_id):
+    user = User.query.get_or_404(user_id)
+
+    return jsonify({
+        "id": user.id,
+        "email": user.email,
+        "full_name": user.full_name,
+        "address": user.shipping_address,
+        "payment_info": user.payment_method_mock or {}
+    })
+
+# to update profile details
+@user_bp.route('/api/users/<int:user_id>', methods=['PUT'])
+def update_user_profile(user_id):
+    user = User.query.get_or_404(user_id)
+    data = request.get_json()
+
+    if 'full_name' in data:
+        user.full_name = data['full_name']
+    if 'address' in data:
+        user.shipping_address = data['address']
+    if 'payment_info' in data:
+        user.payment_method_mock = data['payment_info']
+    
+    try:
+        db.session.commit()
+        return jsonify({"message":"Proflie updated"})
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"message": str(e)}), 500
